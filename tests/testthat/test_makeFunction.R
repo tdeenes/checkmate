6654166aef22e753b6c0095cafef9d85147037a3
Denes Toth <toth.denes@kogentum.hu>
2025-11-11 15:28:22 +0100

    Improve makeXFunctions
    This PR fixes several issues reported [here](https://github.com/mllg/checkmate/issues/281), [here](https://github.com/mllg/checkmate/issues/283), and [here](https://github.com/mllg/checkmate/issues/284).
    
    All these bugs are related to the `makeXFunction` variants, which share a lot of code and use the `parse(eval(...))` construct internally.
    
    This PR introduces:
    1. A function factory for all three `makeXFunction` variants;
    2. Uses R's metaprogramming capabilities to generate the functions without using `parse(eval(...))`;
    3. Adds unit tests to cover the reported issues.
    
    Resolves #281, resolves #283, resolves #284.

--- tests/testthat/test_makeFunction.R
+++ tests/testthat/test_makeFunction.R
@@ -13,6 +13,16 @@ test_that("makeAssertion", {
   expect_identical(formals(x), formals(y))
   if (!isNamespaceLoaded("covr"))
     expect_equal(body(x), body(y))
+
+  check_fn_with_dots = function(x, ..., force.fail = FALSE) {
+    if (isTRUE(force.fail)) {
+      return("Forced failure")
+    }
+    TRUE
+  }
+  assert_fn = makeAssertionFunction(check_fn_with_dots, use.namespace = FALSE)
+  expect_identical(assert_fn(1L), 1L)
+  expect_error(assert_fn(1L, force.fail = TRUE), "Forced failure")
 })
 
 test_that("makeTest", {
@@ -49,21 +59,32 @@ test_that("makeExpectation", {
 })
 
 test_that("makeX with name for 'x' not 'x'", {
-  checker = function(foo, bar = TRUE) check_numeric(foo)
+  checker = function(foo, bar = FALSE) checkFlag(foo, na.ok = bar)
 
   achecker = makeAssertionFunction(checker)
   expect_identical(names(formals(achecker)), c("foo", "bar", ".var.name", "add"))
-  expect_identical(as.character(formals(achecker)$.var.name)[2], "foo")
-  expect_equal(sum(grepl("foo", as.character(body(achecker)))), 3L)
-  expect_equal(sum(grepl("bar", as.character(body(achecker)))), 1L)
+  expect_error(achecker(), 'argument "foo" is missing')
+  expect_identical(achecker(FALSE), FALSE)
+  expect_error(achecker(1L), "Assertion on '1L' failed")
+  expect_error(achecker(NA), "May not be NA")
+  expect_identical(achecker(NA, bar = TRUE), NA)
+  expect_error(achecker(NA, bar = "x"), "'na.ok' must be a flag")
 
   tchecker = makeTestFunction(checker)
   expect_identical(names(formals(tchecker)), c("foo", "bar"))
-  expect_equal(sum(grepl("foo", as.character(body(tchecker)))), 1L)
-  expect_equal(sum(grepl("bar", as.character(body(tchecker)))), 1L)
+  expect_error(tchecker(), 'argument "foo" is missing')
+  expect_identical(tchecker(FALSE), TRUE)
+  expect_identical(tchecker(1L), FALSE)
+  expect_identical(tchecker(NA), FALSE)
+  expect_identical(tchecker(NA, bar = TRUE), TRUE)
+  expect_error(tchecker(NA, bar = "x"), "'na.ok' must be a flag")
 
   echecker = makeExpectationFunction(checker)
   expect_identical(names(formals(echecker)), c("foo", "bar", "info", "label"))
-  expect_equal(sum(grepl("foo", as.character(body(echecker)))), 3L)
-  expect_equal(sum(grepl("bar", as.character(body(echecker)))), 1L)
+  expect_error(echecker(), 'argument "foo" is missing')
+  expect_identical(echecker(FALSE), FALSE)
+  expect_error(echecker(1L), "Check on '1L' failed")
+  expect_error(echecker(NA), "May not be NA")
+  expect_identical(echecker(NA, bar = TRUE), NA)
+  expect_error(echecker(NA, bar = "x"), "'na.ok' must be a flag")
 })
